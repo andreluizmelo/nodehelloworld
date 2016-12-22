490613ec7765584b0c54e3d2cec4ecd391ae23ed
Andre Luizm <andreluiz.melo@outlook.com>
2016-12-22 20:25:02 -0200

    Hello World

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 var express = require("express");
 
+var app = express();
+
+var port = 3000;
+
+app.get('/',function(request, response){
+    response.send('hello world');
+});
+
+app.listen(port, function(err){
+    console.log('Running on port ' + port);
+});
