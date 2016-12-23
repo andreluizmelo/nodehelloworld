296a40b0a0be6e56f1f1fa0bff5f69306a65af76
Andre Luizm <andreluiz.melo@outlook.com>
2016-12-22 23:48:39 -0200

    Porta Padrao 80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80;
 app.use(express.static('public'));
 app.set('views','./src/views');
 app.set('view engine', 'ejs');
