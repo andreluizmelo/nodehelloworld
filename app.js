7d3b3c2828280bd05623495396222c3f4c0d70c0
Andre Luizm <andreluiz.melo@outlook.com>
2016-12-22 22:19:32 -0200

    gulp font-awesome ohmygod

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var express = require("express");
+var express = require('express');
 
 var app = express();
 
 var port = 3000;
 app.use(express.static('public'));
+app.use(express.static('src/views'));
 app.get('/',function(request, response){
     response.send('hello world');
 });
