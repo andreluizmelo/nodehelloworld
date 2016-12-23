f3fbcd1aa25a5876e58fba98e396137c32a4b332
Andre Luizm <andreluiz.melo@outlook.com>
2016-12-22 23:21:48 -0200

    esqueci o que fiz

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var express = require('express');
 
 var app = express();
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views','./src/views');
+app.set('view engine', '');
+
 app.get('/',function(request, response){
     response.send('hello world');
+});
+
+app.get('/hot',function(request, response){
+    response.send('hello world, damn its hot');
 });
 
 app.listen(port, function(err) {
